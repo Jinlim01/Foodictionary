0560c3a6e4f09f4d1a9362ef9e2e6e879e4bbc8c
Jin <jinlim0101@hotmail.com>
2017-10-22 18:20:38 +0100

    navbar

---
+++ navbar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/navbar.css" rel="stylesheet" type="text/css"/>
+        <!--Font Awesome css-->
+        <link href="vendor/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+    </head>
+    <body id="page-top">
+        <nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="background-color: #fff; border-bottom-color: #fff">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="home.php" id="title">FOODICTIONARY</a>
+                </div> 
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right" id="title-right">
+                        <li class="option">
+                            <a href=""><i class="fa fa-user" aria-hidden="true"></i>&nbsp;&nbsp;Guest</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
