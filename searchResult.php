d03211279c89c950a27cde07235d0d1cfa938506
Yuan Jin <Yuan Jin@192.168.1.15>
2017-11-16 17:40:54 +0000

    search result page

---
+++ searchResult.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/homePage.css" rel="stylesheet" type="text/css"/>
+        <!--Font Awesome css-->
+        <link href="vendor/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+        <style>
+            .filter-box{
+                margin-top: 100px;
+                border-radius: 30px;
+                border-style: outset;
+                border-color: #cecece;
+                padding: 10px;
+            }
+
+            .filter-btn span{
+                color: #000;
+                font-family: cursive;
+            }
+            
+            .filter-btn:hover{
+                background-color: #eb3812;
+                transition-duration: 0.4s;
+            }
+            
+            .filter-btn:hover span{
+                color: #fff;
+                transition-duration: 0.4s;
+            }
+        </style>
+    </head>
+    <body>
+        <?php include 'navbar.php' ?>
+
+        <div class="container-fluid option-bar" style="background-color: #eb3812;">
+            <div class="col-md-2 col-sm-2 col-md-offset-3 col-sm-offset-3 option-list">
+                <center><a href="">Cook in 30 Minutes</a></center>
+            </div>
+            <div class="col-md-2 col-sm-2 option-list">
+                <center><a href="">Pick A Meal</a></center>
+            </div>
+            <div class="col-md-2 col-sm-2 option-list">
+                <center><a href="">Top Rated</a></center>
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="col-md-8 col-lg-8 col-sm-8 menu-list">
+                <div class="col-lg-3 col-md-3 col-sm-8 col-md-offset-1 col-sm-offset-2 col-lg-offset-1 menu">
+                    <a href="#"><img class="img-responsive recipe-img" src="img/salmon.jpg"></a>
+                    <div class="rating"><span>Rating:<br>8.1/10.0</span></div>
+
+                    <div class="card-body">
+                        <h4 class="card-title"><a href="#">Teriyaki Salmon</a> </h4>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Filter Box -->
+            <div class="col-md-3 col-lg-3 col-sm-3 filter-box">
+                <p>Filter Box Content</p>
+                <p>Filter Box Content</p>
+                <p>Filter Box Content</p>
+                <p>Filter Box Content</p>
+                <p>Filter Box Content</p>
+                <p>Filter Box Content</p>
+                <p>Filter Box Content</p>
+
+                <center><button type="button" class="btn filter-btn"><span>Search</span></button></center>
+            </div>
+        </div>
+    </body>
+</html>
+
