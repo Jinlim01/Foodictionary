8fef88da3b426cbc8508a9f296baca921f45710c
Yuan Jin <Yuan Jin@192.168.1.15>
2017-11-17 10:09:40 +0000

    search result page

---
+++ recipePage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/recipePage.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container-fluid pic-container">
+            <a><img class="img-responsive img-test" src="img/test.jpg" alt=""/></a>
+        </div>
+    </body>
+</html>
