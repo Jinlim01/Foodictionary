1e5783f18c6fe77ee5943c0d6ff91f111bc8d1f2
limchengbin <callmebin95@gmail.com>
2017-11-17 21:54:00 +0800

    bin

--- searchResult.php
+++ searchResult.php
@@ -1,7 +1,24 @@
+<?php
+    require_once('bin/filter.php');
+    
+
+    $query3 = "SELECT * FROM food_category";
+    $statement3 = $db->prepare($query3);
+    $statement3->execute();
+    $list3 = $statement3->fetchAll();
+    $statement3->closeCursor();
+
+
+    $query4 = "SELECT * FROM food_type";
+    $statement4 = $db->prepare($query4);
+    $statement4->execute();
+    $list4 = $statement4->fetchAll();
+    $statement4->closeCursor();
+?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Home</title>
+        <title>Search</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <link href="css/homePage.css" rel="stylesheet" type="text/css"/>
@@ -51,27 +68,42 @@
 
         <div class="container-fluid">
             <div class="col-md-8 col-lg-8 col-sm-8 menu-list">
-                <div class="col-lg-3 col-md-3 col-sm-8 col-md-offset-1 col-sm-offset-2 col-lg-offset-1 menu">
-                    <a href="#"><img class="img-responsive recipe-img" src="img/salmon.jpg"></a>
-                    <div class="rating"><span>Rating:<br>8.1/10.0</span></div>
+                <?php
+                if(!empty($list)){
+                    foreach($list as $list){
+                        echo '<div class="col-lg-3 col-md-3 col-sm-8 col-md-offset-1 col-sm-offset-2 col-lg-offset-1 menu">
+                              <a href="#"><img class="img-responsive recipe-img" src="img/'.$list['image'].'.jpg"></a>
+                              <div class="rating"><span>Rating:<br>'.$list['rating_number'].'/10.0</span></div>
 
-                    <div class="card-body">
-                        <h4 class="card-title"><a href="#">Teriyaki Salmon</a> </h4>
-                    </div>
-                </div>
+                              <div class="card-body">
+                              <h4 class="card-title"><a href="#">'.$list['food_name'].'</a> </h4>
+                              </div>
+                              </div>';
+                    
+                }}else{
+                    echo "No Recipe Found";
+                }
+                ?>
+                
             </div>
-
+            
             <!-- Filter Box -->
             <div class="col-md-3 col-lg-3 col-sm-3 filter-box">
-                <p>Filter Box Content</p>
-                <p>Filter Box Content</p>
-                <p>Filter Box Content</p>
-                <p>Filter Box Content</p>
-                <p>Filter Box Content</p>
-                <p>Filter Box Content</p>
-                <p>Filter Box Content</p>
-
-                <center><button type="button" class="btn filter-btn"><span>Search</span></button></center>
+                <form action="searchResult.php" method="post">
+                    <?php
+                        foreach ($list3 as $list3) {
+                            echo '<input type="radio" value="'.$list3['food_category_id'].'" name="style">'.$list3['food_category_name'].'<br>';
+                        }
+                    ?>
+                    <hr>
+                    <?php
+                        foreach ($list4 as $list4) {
+                            echo '<input type="checkbox" value="'.$list4['food_type_id'].'" name=type[]">'.$list4['food_type_name'].'<br>';
+                        }
+                    ?>
+                    
+                    <input type="submit">
+                </form>
             </div>
         </div>
     </body>
